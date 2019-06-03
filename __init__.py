fb502d017a7edfb806507cf90d9541f9cb6639f9
bradyz <brady.zhou@utexas.edu>
2019-06-03 09:53:22 -0700

    update

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
+import json
+
 from collections import defaultdict
 
 from . import hdf5_maker
 from . import video_maker
 from . import gif_maker
+from . import saver
 
 save_hdf5 = hdf5_maker.save_hdf5
+HDF5Dataset = hdf5_maker.HDF5Dataset
 
 show_image = video_maker.show
 init_video = video_maker.init
@@ -15,3 +19,22 @@ save_gif = gif_maker.save
 clear_gif = gif_maker.clear
 
 dictlist = lambda: defaultdict(list)
+
+log = saver.Experiment()
+
+
+def load_json(path):
+    def _cast(x):
+        try:
+            return float(x)
+        except:
+            return str(x)
+
+
+    with open(path, 'r') as f:
+        data = json.load(f)
+
+    for key in data:
+        data[key] = _cast(data[key])
+
+    return data
